2c275496867dfdf4c1a78d8081c58eddca8f9d67
tdurnford <tjdford@gmail.com>
2017-10-21 15:41:16 -0700

    Spring modifications

--- routes/api.js
+++ routes/api.js
@@ -6,11 +6,11 @@ var product = require('cartesian-product');
 
 // GETS all Subject, Course Number, and Title combinations from Catalog
 router.get('/classes', function (req, res) {
-    Catalog.aggregate({"$group" : {_id : {Subject:"$Subject", Course:"$Course", Title:"$Title", Credits:"$Credits", Class:"$Class"}}}
+    Catalog.aggregate({"$group" : {_id : {subject:"$subject", course:"$course", title:"$title", credits:"$credits", seminar:"$seminar"}}}
         ).then(function (classes) {
             var results = classes.map(function (result) { return result._id });
-            results.sort(function(a,b) {return (a.Subject > b.Subject) ? 1 : ((b.Subject > a.Subject) ? -1
-                :(a.Course > b.Course) ? 1 :(a.Course < b.Course)? -1: 0);} );
+            results.sort(function(a,b) {return (a.subject > b.subject) ? 1 : ((b.subject > a.subject) ? -1
+                :(a.course > b.course) ? 1 :(a.course < b.course)? -1: 0);} );
         res.send(results)
     })
 });
@@ -18,7 +18,7 @@ router.get('/classes', function (req, res) {
 router.post('/sections', function(req, res){
     course = req.body.class;
 
-    Catalog.find({Class: course}).then(function (sections) {
+    Catalog.find({seminar: course}).then(function (sections) {
         res.send(sections);
     });
 });
@@ -27,15 +27,17 @@ router.post('/sections', function(req, res){
 router.post('/schedules', function(req, res){
     classes = req.body.classes;
     blocks = req.body.blocks;
-    Catalog.find({Class:{$in:classes}}).then(function (sections) {
+    Catalog.find({seminar:{$in:classes}}).then(function (sections) {
 
         var results = {};
         // initialize results dictionary
         classes.map(function (elem) {results[elem] = []});
         // sort sections into dictionary by Class data field
-        sections.map(function (section) {results[section.Class].push(section);});
+        sections.map(function (section) {results[section.seminar].push(section);});
         configured = Object.keys(results).map(function (key) {return results[key]});
-        all_schedules = product(configured).map(function (schedule) { return schedule }); //.concat(blocks)
+
+        all_schedules = product(configured).map(function (schedule) {return schedule});
+        all_schedules = all_schedules.map(function (schedule) {return schedule.concat(blocks)});
 
         res.send(viable_schedules(all_schedules))
     })
@@ -50,6 +52,7 @@ function conflicts(week1, week2) {
     // checks if corresponding time ranges
     // from week1 and week2 conflict with each other
     // the results are return in an array
+
     conflict = Object.keys(week1).map(function(key){
         return time_conflicts(week1[key], week2[key]);
     });
@@ -61,6 +64,9 @@ function conflicts(week1, week2) {
     // returns true if time1 conflicts with time2; otherwise false
     function time_conflicts(time1, time2){
         if(time1 == null || time2 == null){return false;}  // check if time1 or time2 are null
+        // convert time from strings to Date Objects
+        time1 = time1.map(function (time) { return new Date("1/18/2017 " + time) });
+        time2 = time2.map(function (time) { return new Date("1/18/2017 " + time) });
         return !(time1[1] <= time2[0] || time2[1] <= time1[0]);
     }
 }
@@ -83,9 +89,10 @@ function viable_schedules(schedules) {
             return true
         } else {
             head = schedule[0]; tail = schedule.slice(1);
+            console.log(head.Class);
 
             for (var i = 0; i < tail.length; i++) {
-                if (conflicts(head.Week, tail[i].Week)) {
+                if (conflicts(head.week, tail[i].week)) {
                     return false
                 }
             }
