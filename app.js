c4d8369038c4fc1396b0687979f5ff31e03062fa
tdurnford <tjdford@gmail.com>
2017-11-09 16:26:37 -0800

    throng

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 var api = require('./routes/api');
 require('dotenv').config();
 
@@ -44,15 +42,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use('/', index);
-app.use('/users', users);
 app.use('/api', api);
 
 app.use(function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-})
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
